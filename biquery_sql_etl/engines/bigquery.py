045e7f6826022ad3ed19d40958c059ff52f5dc8f
Todd Birchard <toddbirchard@gmail.com>
2020-02-11 03:26:52 -0500

    tweaks

--- biquery_sql_etl/engines/bigquery.py
+++ biquery_sql_etl/engines/bigquery.py
@@ -1,11 +1,8 @@
+"""SQL Database Engine."""
+
 from sqlalchemy.engine import create_engine
 from config import (gcp_credentials,
                     bigquery_uri)
 
 
-def create_bigquery_engine(uri, creds):
-    engine = create_engine(uri, credentials_path=creds)
-    return engine
-
-
-bigquery_engine = create_bigquery_engine(bigquery_uri, gcp_credentials)
+bigquery_engine = create_engine(bigquery_uri, credentials_path=gcp_credentials)
--- biquery_sql_etl/engines/rdbms.py
+++ biquery_sql_etl/engines/rdbms.py
@@ -1,10 +1,6 @@
-"""SQL Database Client."""
+"""SQL Database Engine."""
 from sqlalchemy.engine import create_engine
 from config import rdbms_uri
 
 
-def create_rdbms_engine(uri):
-    return create_engine(uri)
-
-
-rdbms_engine = create_rdbms_engine(rdbms_uri)
+rdbms_engine = create_engine(rdbms_uri)
